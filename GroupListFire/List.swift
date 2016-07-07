d12019a5b5066d54b5e49615c96f1814d744c481
tedmarchildon <tmm61@duke.edu>
2016-07-07 12:02:02 -0600

    I changed a lot of stuff

---
+++ GroupListFire/List.swift
@@ -0,0 +1,38 @@
+//
+//  List.swift
+//  GroupListFire
+//
+//  Created by Teddy Marchildon on 7/7/16.
+//  Copyright © 2016 Teddy Marchildon. All rights reserved.
+//
+
+import Foundation
+
+class List {
+    
+    var items: [String] = []
+    
+    init(list: [String]) {
+        self.items = list
+    }
+    
+    func addItem(item: String) {
+        items.append(item)
+    }
+    
+    func addAllItems(items: [String]) {
+        self.items.appendContentsOf(items)
+    }
+    
+    func removeItem(item: String) {
+        var dex = 0
+        for elem in items {
+            if elem == item {
+                items.removeAtIndex(dex)
+                return
+            } else {
+                dex += 1
+            }
+        }
+    }
+}
