c1d54048d81fe6f3ea9b9d27f879bc12c96945f6
tedmarchildon <tmm61@duke.edu>
2016-08-01 15:47:09 -0600

    Deleted unnecessary comments

--- GroupListFire/UserSearchTableViewController.swift
+++ GroupListFire/UserSearchTableViewController.swift
@@ -105,9 +105,7 @@ class UserSearchTableViewController: UITableViewController, UISearchResultsUpdat
         navigationController?.popViewControllerAnimated(true)
     }
     
-    // Override to support conditional editing of the table view.
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
         return false
     }
     
